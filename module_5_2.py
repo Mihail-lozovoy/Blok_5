03a42b9995340720c15a0e9927e57729c0ec9d4e
Mihail-lozovoy <lozovoy_michael@inbox.ru>
2024-07-05 17:54:55 +0300

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,14 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+print("Название :", h1.name,"Кол-во этажей :", h1.number_of_floors)
+print("Название :", h2.name,"Кол-во этажей :", h2.number_of_floors)
+print(len(h1))
+print(len(h2))
