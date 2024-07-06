b0d2280092daa1b533f155d2ebc7b48ca91842a2
Mihail-lozovoy <lozovoy_michael@inbox.ru>
2024-07-06 13:41:01 +0300

    Add files via upload

--- module_5_2.py
+++ module_5_2.py
@@ -3,16 +3,16 @@ class House:
         self.name = name
         self.number_of_floors = number_of_floors
 
+    def __str__(self):
+        return (f"Название: {self.name}, Кол-во этажей: {self.number_of_floors}")
     def __len__(self):
         return self.number_of_floors
 
-    def __str__(self):
-        return self.name
 
-h1 = House('ЖК Эльбрус', 10)
-h2 = House('ЖК Акация', 20)
-print("Название :", h1.name,"Кол-во этажей :", h1.number_of_floors)
-print("Название :", h2.name,"Кол-во этажей :", h2.number_of_floors)
+
+h1 = House('ЖК Эльбрус,', 10)
+h2 = House('ЖК Акация,', 20)
+
 print(h1)
 print(h2)
 print(len(h1))
