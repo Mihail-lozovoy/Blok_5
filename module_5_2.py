6432c265f471a40f2073179af862c23a991f0415
Mihail-lozovoy <lozovoy_michael@inbox.ru>
2024-07-06 12:47:46 +0300

    Update module_5_2.py

--- module_5_2.py
+++ module_5_2.py
@@ -6,9 +6,14 @@ class House:
     def __len__(self):
         return self.number_of_floors
 
+    def __str__(self):
+        return self.name
+
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
 print("Название :", h1.name,"Кол-во этажей :", h1.number_of_floors)
 print("Название :", h2.name,"Кол-во этажей :", h2.number_of_floors)
+print(h1)
+print(h2)
 print(len(h1))
 print(len(h2))
