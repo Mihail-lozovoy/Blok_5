af1c9196072d865b293fdaebe4f68f5449080319
Mihail-lozovoy <lozovoy_michael@inbox.ru>
2024-07-08 01:22:07 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -1,55 +1,25 @@
 class House:
-    def __init__(self, name, number_of_floor):
+    houses_history = []
+
+    def __new__(cls, *args):
+        houses_history = super().__new__(cls)
+        cls.houses_history.append(args[0])
+
+        return houses_history
+
+    def __init__(self, name, number_of_floors):
         self.name = name
-        self.number_of_floor = number_of_floor
+        self.number_of_floors = number_of_floors
 
-    def __eq__(self, other):
-        return self.number_of_floor == other.number_of_floor
-
-    def __lt__(self, other):
-        return self.number_of_floor < other.number_of_floor
-
-    def __le__(self, other):
-        return self.number_of_floor <= other.number_of_floor
-
-    def __gt__(self, other):
-        return self.number_of_floor > other.number_of_floor
-
-    def __ge__(self, other):
-        return self.number_of_floor >= other.number_of_floor
-
-    def __ne__(self, other):
-        return self.number_of_floor != other.number_of_floor
-
-    def __add__(self, other):
-        self.number_of_floor = self.number_of_floor + other
-        return self
-
-    def __radd__(self, other):
-         return self.__add__(other)
-
-    def __iadd__(self, other):
-        self.number_of_floor += other
-        return self
-
-    def __str__(self):
-        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floor}')
+    def __del__(self):
+      print(f'{self.name}: Снесён, но он останется в истории')
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
-
-print(h1)
-print(h2)
-print(h1 == h2)
-h1 = h1 + 10
-print(h1)
-print(h1 == h2)
-h1 += 10
-print(h1)
-h2 = 10 + h2
-print(h2)
-print(h1 > h2)
-print(h1 >= h2)
-print(h1 < h2)
-print(h1 <= h2)
-print(h1 != h2)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+del h2
+del h3
+print(House.houses_history)
